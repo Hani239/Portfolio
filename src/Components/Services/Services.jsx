e4cceda86bb018657b8aee902632cf685db3d3a8
Hani239 <hanivinaben2924@gmail.com>
2025-05-18 16:49:57 +0530

    Service section created

---
+++ src/Components/Services/Services.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Services.css'
+import theme_pattern from '../../assets/curve.png'
+import Services_Data from '../../assets/services_data'
+import { HiArrowNarrowRight } from "react-icons/hi";
+
+const Services = () => {
+  return (
+    <div className='services'>
+        <div className="services-title">
+            <h1>My Services</h1>
+            <img src={theme_pattern} alt="theme patern" />
+        </div>
+        <div className="services-container">
+            {Services_Data.map((service, index)=>{
+                return <div key={index} className='services-format'>
+                    <h3>{service.s_no}</h3>
+                    <h2>{service.s_name}</h2>
+                    <p>{service.s_desc}</p>
+                    <div className="services-readmore">
+                        <p>Read More</p>
+                        <HiArrowNarrowRight className='font'/>
+                    </div>
+                </div>
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default Services
---
+++ src/assets/services_data.js
@@ -0,0 +1,34 @@
+const Services_Data = [
+    {
+        s_no:"01",
+        s_name:"Web design",
+        s_desc:"Designing clean, responsive websites that balance user experience and visual appeal across all devices and screen sizes."
+    },
+    {
+        s_no:"02",
+        s_name:"Graphics design",
+        s_desc:"Designing eye-catching visuals, logos, and social media creatives that align with brand identity and communication goals."
+    },
+    {
+        s_no:"03",
+        s_name:"Social media",
+        s_desc:"Designing eye-catching posts, stories, and reels aligned with brand tone and marketing objectives."
+    },
+    {
+        s_no:"04",
+        s_name:"App design",
+        s_desc:"Designing intuitive and user-friendly mobile app interfaces focused on clean layout, usability, and responsiveness."
+    },
+    {
+        s_no:"05",
+        s_name:"Digital marketing",
+        s_desc:"Managing SEO, social media, and email marketing strategies to drive online growth and customer engagement."
+    },
+    {
+        s_no:"06",
+        s_name:"Content writing",
+        s_desc:"Researching topics thoroughly to produce accurate, compelling articles that attract and retain readers."
+    },
+]
+
+export default Services_Data;
