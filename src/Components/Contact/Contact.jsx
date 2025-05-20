a8dcd867f50e76c29c4d1c184609e37f897483b9
Hani239 <hanivinaben2924@gmail.com>
2025-05-21 03:34:12 +0530

    Contact section created

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import './Contact.css'
+import theme_pattern from '../../assets/curve.png'
+import { IoMail } from "react-icons/io5";
+import { FaLinkedin } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+
+const Contact = () => {
+    return (
+        <div className='contact'>
+            <div className="contact-title">
+                <h1>Contact me</h1>
+                <img src={theme_pattern} alt="theme patern" />
+            </div>
+            <div className="contact-section">
+                <div className="contact-left">
+                    <h1>Let&apos;s talk</h1>
+                    <p>Got a project in mind? I&apos;m always open to new opportunities and exciting ideas. Drop me a message below and let&apos;s connect!</p>
+                    <div className="contact-details">
+                        <div className="contact-detail">
+                            <IoMail />
+                            <a
+                                href="mailto:hanizalain9@gmail.com"
+                            >
+                                hanizalain9@gmail.com
+                            </a></div>
+                        <div className="contact-detail">
+                            <FaLinkedin />
+                            <a
+                                href="https://www.linkedin.com/in/hani-zala/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                https://www.linkedin.com/in/hani-zala/
+                            </a></div>
+                        <div className="contact-detail"><FaLocationDot /><p>Gandhinagar, Gujarat, India</p></div>
+                    </div>
+                </div>
+                <form className="contact-right">
+                    <label htmlFor="">Your Name</label>
+                    <input type="text" placeholder='Enter your name' name='name' />
+                    <label htmlFor="">Your Email</label>
+                    <input type="text" placeholder='Enter your email' name='email' />
+                    <label htmlFor="">Write your message here</label>
+                    <textarea name="message" rows="8" placeholder='Enter your message'></textarea>
+                    <button type='submit' className="contact-submit">Submit now</button>
+
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Contact
