ba4ad3343276b54d90ffdb7e477b1dc2d1d16a29
Hani239 <hanivinaben2924@gmail.com>
2025-05-21 02:02:21 +0530

    MyWork section created

---
+++ src/Components/MyWork/MyWork.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './MyWork.css'
+import theme_pattern from '../../assets/curve.png'
+import mywork_data from '../../assets/mywork_data'
+import { HiArrowNarrowRight } from "react-icons/hi";
+
+const MyWork = () => {
+  return (
+    <div className='mywork'>
+      <div className="mywork-title">
+        <h1>My Latest Work</h1>
+        <img src={theme_pattern} alt="theme patern" />
+      </div>
+      <div className="mywork-container">
+        {mywork_data.map((work, index) => (
+          <div className="work-item" key={index} onClick={() => window.open(work.w_link, "_blank")}>
+            <img src={work.w_img} alt={work.w_name} />
+            <div className="work-overlay">Click to view</div>
+          </div>
+        ))}
+      </div>
+      {/* <div className="mywork-showmore">
+        <p>Show more</p>
+        <HiArrowNarrowRight className='font' />
+      </div> */}
+    </div>
+  )
+}
+
+export default MyWork
---
+++ src/assets/mywork_data.js
@@ -0,0 +1,44 @@
+import project1_img from '../assets/Project1.png'
+import project2_img from '../assets/Project2.png'
+import project3_img from '../assets/Project3.png'
+
+const mywork_data = [
+    {
+        w_no: 1,
+        w_name: "Baby clothes e-commerce website",
+        w_img: project1_img,
+        w_link: "https://baby-e-com.vercel.app/"
+    },
+    {
+        w_no: 2,
+        w_name: "Paradise Pulse - Resort Management website",
+        w_img: project2_img,
+        w_link: "https://paradise-pulse.vercel.app/"
+    },
+    {
+        w_no: 3,
+        w_name: "VegBazaar - Online Grocery website",
+        w_img: project3_img,
+        w_link: "https://paradise-pulse.vercel.app/"
+    },
+    {
+        w_no: 4,
+        w_name: "Baby clothes e-commerce website",
+        w_img: project1_img,
+        w_link: "https://paradise-pulse.vercel.app/"
+    },
+    {
+        w_no: 5,
+        w_name: "Paradise Pulse - Resort Management website",
+        w_img: project2_img,
+        w_link: "https://paradise-pulse.vercel.app/"
+    },
+    {
+        w_no: 6,
+        w_name: "VegBazaar - Online Grocery website",
+        w_img: project3_img,
+        w_link: "https://paradise-pulse.vercel.app/"
+    },
+]
+
+export default mywork_data;
